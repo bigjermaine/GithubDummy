4667ee235eeebd7ad7500985baf15a2296875329
BigJermaine <113020989+bigjermaine@users.noreply.github.com>
2024-09-25 02:57:32 +0100

    Feature(CreatedMainTabbarViewController): created main tabbar view controller to handle several controllers

---
+++ GithubDummy/Controllers/Main/MainTabViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainTabViewController.swift
+//  GithubDummy
+//
+//  Created by MacBook AIR on 25/09/2024.
+//
+
+import UIKit
+
+class MainTabViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GithubDummy/Manager /HapticManager.swift
@@ -0,0 +1,36 @@
+//
+//  HapticManager.swift
+//  TonWallet
+//
+//  Created by MacBook AIR on 03/05/2024.
+//
+
+
+import Foundation
+import UIKit
+
+
+final class HapticManager {
+ static   let shared = HapticManager()
+    
+    
+    private init() {}
+    
+    public func vibrateForSelection() {
+        DispatchQueue.main.async {
+            let generator = UISelectionFeedbackGenerator()
+            generator.prepare()
+            generator.selectionChanged()
+        }
+    }
+    
+    public func vibrate(for type:UINotificationFeedbackGenerator.FeedbackType) {
+        DispatchQueue.main.async {
+            let generator =  UINotificationFeedbackGenerator()
+            generator.prepare()
+            generator.notificationOccurred(type)
+        }
+    }
+    
+    
+}
